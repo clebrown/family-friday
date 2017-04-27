1e698880a8c33215d0f184d6252f6ea0be9440ca
Alicia B <clebrown@adobe.com>
2017-04-27 11:39:41 -0700

    setup routes for managing employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,26 @@
 class EmployeesController < ApplicationController
   def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(user_params)
+    redirect_to root_path
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+    render :show
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:title, :first_name, :last_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
  Rails.application.routes.draw do
   root to: "employees#index"
+
+  get "/employees", to: "employees#:id"
+  get "employees/new", to: "employes#new"
+  post "/employees", to: "employees#create"
   end
